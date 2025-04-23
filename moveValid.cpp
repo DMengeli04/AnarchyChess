571d3377ea8a3bb605dca2e875b4c5266d264e08
DMengeli04 <dmengeli@vols.utk.edu>
2025-04-23 09:08:52 -0400

    pawn rules updated

--- moveValid.cpp
+++ moveValid.cpp
@@ -87,11 +87,25 @@ bool chessPlayer::isValid(int from, int to) {
         return false;
     }
 
-
+    // pawns can only move double on first move
     if (board[from].letter == 'p' && board[from].move_count > 0 && (to == from - 16|| to == from + 16)) {
         return false;
     }
+	
+// pawns can't move diagonally unless they're capturing
+  if (board[from].letter == 'p' &&
+      (to == from + 9 || to == from - 9 || to == from + 7 || to == from - 7) &&
+      board[to].letter == '_') {
+    return false;
+  }
 
+  // pawns can't capture on a forward move
+  if (board[from].letter == 'p' &&
+      (to == from + 8 || to == from - 8 || to == from + 16 ||
+       to == from - 16) &&
+      board[to].letter != '_') {
+    return false;
+  }
 
     //If its white's turn and if the piece is not white, then return false
     if (white && colors[from] != 'w') {
