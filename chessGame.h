9527eda23b04ff4af287db8a84c3410ee4cdb0bc
spatel787 <siddh381997@gmail.com>
2025-05-06 09:37:13 -0400

    added chess player class to chessGame.h

--- chessGame.h
+++ chessGame.h
@@ -1,9 +1,10 @@
-// written by Davyn Mengeling
+// written by Davyn Mengeling and edited by Siddh Patel
 
 #ifndef PIECE_H
 #define PIECE_H
 
 #include <vector>
+#include <string>
 
 // initiate class to store pieces
 class Piece {
@@ -15,6 +16,26 @@ public:
   std::vector<int> moves;
   int move_count;
   Piece &operator=(const Piece &p);
+};
+
+class chessPlayer {
+public:
+  std::vector<Piece> board;
+  std::vector<char> colors; // This if for tracking the colors of pieces
+  bool white, castle;
+  chessPlayer();
+  int chessNotationToIndex(std::string position);
+  bool isValid(int from, int to);
+  void moveMaker(int from, int to);
+  bool findPath(int from, int to);
+  void printBoard();
+  void gamePlayLoop();
+  bool King_Under_Check();
+  bool king_can_be_in_check(int from, int to);
+  void handlePromotion(int position);
+  bool canCastle(int king, int rook);
+  void performCastling(int king, int rook);
+  int enPassant;
 };
 
 #endif
