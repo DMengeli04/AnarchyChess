4c213723e2c6f076c2d6cbca00b43618e8b03185
spatel787 <siddh381997@gmail.com>
2025-05-06 09:38:17 -0400

    Update chessGame.cpp

--- chessGame.cpp
+++ chessGame.cpp
@@ -197,27 +197,6 @@ std::vector<Piece> generate_board(char mode) {
   return board;
 }
 
-/*
-class chessPlayer {
-public:
-  std::vector<Piece> board;
-  std::vector<char> colors; // This if for tracking the colors of pieces
-  bool white, castle;
-  chessPlayer();
-  int chessNotationToIndex(std::string position);
-  bool isValid(int from, int to);
-  void moveMaker(int from, int to);
-  bool findPath(int from, int to);
-  void printBoard();
-  void gamePlayLoop();
-  bool King_Under_Check();
-  bool king_can_be_in_check(int from, int to);
-  void handlePromotion(int position);
-  bool canCastle(int king, int rook);
-  void performCastling(int king, int rook);
-  int enPassant;
-};*/
-
 // Constructor
 chessPlayer::chessPlayer() {
   char mode;
